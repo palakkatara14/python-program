6e58a995256b8cf31cff204c42f81a152901337c
palakkatara14 <122154471+palakkatara14@users.noreply.github.com>
2023-02-17 00:18:01 +0530

    Update main (9).py

--- main (9).py
+++ main (9).py
@@ -1,6 +1,7 @@
+from math import sqrt , pow
 x1 = int(input('enter the value of x1 '))
 x2 = int(input('enter the value of x2 '))
 y1 =  int(input('enter the value  of y1'))
 y2 =  int(input('enter the value of y2'))
-distance = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)
-print(distance)
+distance =  sqrt(pow((x2-x1),2)+pow((y2-y1),2))
+print("distance between two points",round(distance,2))
