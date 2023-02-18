e34de7a1a1a450c0426e3e1d7877bc72bf2f96ed
palakkatara14 <122154471+palakkatara14@users.noreply.github.com>
2023-02-18 22:40:11 +0530

    Update and rename main (44).py to right angle or not

--- main (44).py
+++
@@ -1,6 +0,0 @@
-a = int(input("enter number"))
-b = int(input("enter number"))
-c = int(input("enter number"))
-d = a+b+c==180
-out = 'right  angle triangle'*d + 'not right triangle'*(1-d)
-print(out)
