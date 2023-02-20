5cdfb774f343b79e11872733b062aa24e3f59dfb
palakkatara14 <122154471+palakkatara14@users.noreply.github.com>
2023-02-21 00:20:15 +0530

    Update calculate electricity bil.py

--- calculate electricity bil.py
+++ calculate electricity bil.py
@@ -5,7 +5,7 @@ elif unit>50 and unit<=150:
      bill = unit*0.50+(unit-50)*0.75
 elif unit>150 and unit<=250:
      bill = bill*0.50+100*0.75+(unit-150)*1.25
-elif unit>=250:
+elif unit>250:
      bill = bill*0.50+100*0.75+100*1.25+(unit-250)*1.50
 bill = bill+bill*0.20
 print(bill)
