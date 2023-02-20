9502b65c58d99add8e4706abca1ca491872e616f
palakkatara14 <122154471+palakkatara14@users.noreply.github.com>
2023-02-21 00:21:26 +0530

    Add files via upload

---
+++ calculate gross salary.py
@@ -0,0 +1,13 @@
+salary =  int(input("enter basic salary"))
+if salary<=10000:
+    gross = (20/100)*10000+(80/100)*10000+10000
+    
+    print(gross)
+elif salary<=20000:
+     gross  = (25/100)*20000+(90/100)*20000+20000
+     print(gross)
+elif salary>20000:
+     gross  = (30/100)*20000+(95/100)*20000+20000
+     print(gross)
+else:
+    print("nothing")
